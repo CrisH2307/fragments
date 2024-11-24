8c0819e3591533c38c2a7cb3165f86a01d765bb4
CrisH2307 <khangpicasso@gmail.com>
2024-11-24 17:52:51 -0500

    Updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // version and author from package.json
-const { version, author } = require('../../package.json');
+const { version } = require('../../package.json');
 const { hostname } = require('os');
 
 // Create a router that we can use to mount our API
